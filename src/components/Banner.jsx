0ceaaa38b39302c0ef88537bcb4f14ed3fe7a1d2
Arthur Reis <129776400+friarthur@users.noreply.github.com>
2025-07-07 23:00:47 -0300

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,7 +5,7 @@ import "./Banner.css";
 const Banner = () => {
   return (
     <div className="Banner">
-      <img className="bg" src="/public/Banner.jpg" alt="Fundo" />
+      <img className="bg" src="/Banner.jpg" alt="Fundo" />
       <div className="text">
         <img className="profile" src="/Profile.png" alt="Perfil" />
         <h1>Arthur Reis Mendes</h1>
